24c4691f6c62237c1d4082d4ced9c1d70d473448
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-21 17:59:01 +0600

    end0foffice

--- src/App.js
+++ src/App.js
@@ -8,66 +8,90 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      seconds: '00', 
-      minutes: ''
-    };
-
-  this.secondsRemaining; 
-  this.intervalHandle;
-  this.handleChange = this.handleChange.bind(this);
-
-  // method that triggers the countdown functionality
-  this.startCountDown = this.startCountDown.bind(this);
-  this.tick = this.tick.bind(this);
+      seconds: '00',
+      value: '',
+      isClicked : false
+    }
+    this.secondsRemaining;
+    this.intervalHandle;
+    this.handleChange = this.handleChange.bind(this);
+    this.startCountDown = this.startCountDown.bind(this);
+    this.tick = this.tick.bind(this);
   }
 
   handleChange(event) {
     this.setState({
-      minutes: event.target.value
-    });
+      value: event.target.value
+    })
   }
 
   tick() {
     var min = Math.floor(this.secondsRemaining / 60);
     var sec = this.secondsRemaining - (min * 60);
+
     this.setState({
-      minutes: min,
-      seconds: sec
-    });
+      value: min,
+      seconds: sec,
+    })
 
     if (sec < 10) {
       this.setState({
         seconds: "0" + this.state.seconds,
-      });
+      })
+
     }
-    
+
     if (min < 10) {
       this.setState({
-      value: "0" + min,
-      });
+        value: "0" + min,
+      })
+
     }
-    
+
     if (min === 0 & sec === 0) {
       clearInterval(this.intervalHandle);
     }
 
-    this.secondsRemaining--;
+
+    this.secondsRemaining--
   }
 
   startCountDown() {
     this.intervalHandle = setInterval(this.tick, 1000);
-    let time = this.state.minutes;
+    let time = this.state.value;
     this.secondsRemaining = time * 60;
+    this.setState({
+      isClicked : true
+    })
   }
 
   render() {
+    const clicked = this.state.isClicked;
+    if(clicked){
     return (
-      <div className="App">
-        <TimerInput minutes ={this.state.minutes} handleChange={this.handleChange} />
-        <Timer minutes={this.state.minutes} seconds={this.state.seconds} />
-        <StartButton />
+      <div>
+        <div className="row">
+          <div className="col-md-4"></div>
+          <div className="col-md-4">
+            <Timer value={this.state.value} seconds={this.state.seconds} />
+          </div>
+        </div>
       </div>
     );
+    }else{
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <TimerInput value={this.state.value} handleChange={this.handleChange} />
+              <Timer value={this.state.value} seconds={this.state.seconds} />
+              <StartButton startCountDown={this.startCountDown} value={this.state.value} />
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -4,7 +4,7 @@ class StartButton extends Component {
   render() {
     return (
       <div style={{ marginLeft: 130 }}>
-        <button>Start</button>
+        <button onClick={this.props.startCountDown}>Start</button>
       </div>
     );
   }
