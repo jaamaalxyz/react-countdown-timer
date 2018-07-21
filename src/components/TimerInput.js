4afe5c9d74f90b1186d5f54412103696a9b0c52f
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-21 16:57:54 +0600

    handleChange

--- src/components/TimerInput.js
+++ src/components/TimerInput.js
@@ -5,7 +5,7 @@ class TimerInput extends React.Component {
     return (
       <div style={{marginLeft:100}}>
         <h3>Input your desired time</h3>
-        <input type="number" minutes={this.props.minutes} required />
+        <input type="number" minutes={this.props.minutes} handleChange={this.props.handleChange} required />
       </div>
     );
   }
