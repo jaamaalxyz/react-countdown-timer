e4ce368800ef02dbc7f144b73667608f8a01097d
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-21 15:45:55 +0600

    styled Timer h1

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,7 +4,7 @@ class Timer extends React.Component {
   render() {
     return (
       <div>
-        <h1> </h1>
+        <h1 style={{ fontSize: 100, marginLeft:100 }}> </h1>
       </div>
     );
   }
