3abb8475fd151fb2884ea68d2ebaefadc331cea7
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-21 15:45:15 +0600

    styled TimerInput

--- src/components/TimerInput.js
+++ src/components/TimerInput.js
@@ -3,7 +3,7 @@ import React from 'react';
 class TimerInput extends React.Component {
   render() {
     return (
-      <div>
+      <div style={{marginLeft:100}}>
         <h3>Input your desired time</h3>
         <input type="number" required />
       </div>
