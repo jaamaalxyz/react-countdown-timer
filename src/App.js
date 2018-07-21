366e4beab5952900c68c837317508b7c87d05e61
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-21 15:39:29 +0600

    Timer initialized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import TimerInput from './components/TimerInput';
+import Timer from './components/Timer';
 
 class App extends Component {
   render() {
