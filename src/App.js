a81bddb19211bceef9f89f6b7d6ff1212b6ff77c
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-21 16:50:34 +0600

    create minutes and seconds props

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import Timer from './components/Timer';
 import StartButton from './components/StartButton';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: '00',
+      minutes: ''
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <TimerInput />
-        <Timer />
+        <TimerInput minutes ={this.state.minutes} />
+        <Timer minutes={this.state.minutes} seconds={this.state.seconds} />
         <StartButton />
       </div>
     );
