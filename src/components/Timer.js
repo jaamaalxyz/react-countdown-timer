eabfce1ba0c5ebd0ce16a69a6119d791f4402256
jamal-pb95 <jamal.pb95@gmail.com>
2018-07-21 16:53:46 +0600

    added props to Timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,7 +4,7 @@ class Timer extends React.Component {
   render() {
     return (
       <div>
-        <h1 style={{ fontSize: 100, marginLeft:100 }}> </h1>
+        <h1 style={{ fontSize: 100, marginLeft:100 }}>{this.props.minutes}:{this.props.seconds}</h1>
       </div>
     );
   }
